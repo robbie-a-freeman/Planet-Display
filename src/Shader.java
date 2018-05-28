16ed881ca8bff09e6b0bbbbaef35b0baa51a53b5
Robert Freeman <zarohian@yahoo.com>
2018-05-27 22:13:21 -0400

    Added Uniforms
    Uniforms added in preparation for transofrmations/rotations. Also added Vector and Matrix libraries from https://github.com/SilverTiger/lwjgl3-tutorial/tree/master/src/silvertiger/tutorial/lwjgl/math

--- src/Shader.java
+++ src/Shader.java
@@ -1,6 +1,11 @@
+import org.lwjgl.BufferUtils;
+import org.lwjgl.ovr.OVRVector3f;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.FloatBuffer;
+import java.util.Vector;
 
 import static org.lwjgl.opengl.GL11.GL_FALSE;
 import static org.lwjgl.opengl.GL20.*;
@@ -60,6 +65,34 @@ public class Shader {
         glDeleteShader(vertexShader);
         glDeleteShader(fragmentShader);
         glDeleteProgram(program);
+    }
+
+    protected void getAllUniforms() {
+
+    }
+
+    protected int getUniform(String name) {
+        return glGetUniformLocation(program, name);
+    }
+
+    protected void loadFloatUniform(int location, float value) {
+        glUniform1f(location, value);
+    }
+
+    protected void loadIntUniform(int location, int value) {
+        glUniform1i(location, value);
+    }
+
+    protected void loadVectorUniform(int location, Vector3f value) {
+        glUniform3f(location, value.x, value.y, value.z);
+    }
+
+    protected void loadMatrixUniform(int location, Matrix4f value) {
+        FloatBuffer buffer = BufferUtils.createFloatBuffer(16);
+
+        value.toBuffer(buffer);
+        buffer.flip();
+        glUniformMatrix4fv(location, false, buffer);
     }
 
     public void useShader() {
