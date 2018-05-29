a9f2024b5980a8f984a9120d2fbbee7b1aa2eee0
Robert Freeman <zarohian@yahoo.com>
2018-05-29 17:25:25 -0400

    Adjusted shader again
    Fragment shader now computes distance from center. Background is now black

--- out/production/Planet-Display/resources/shaders/basic.fs
+++ out/production/Planet-Display/resources/shaders/basic.fs
@@ -3,6 +3,9 @@
 out vec4 fragColor;
 
 void main() {
-    fragColor = vec4(0.0f, 1.5f - gl_FragCoord.z, gl_FragCoord.z - 0.5f, 1.0f);
+    float distSq;
+    distSq = (0.0f - gl_FragCoord.x) * (0.0f - gl_FragCoord.x) + (0.0f - gl_FragCoord.y) * (0.0f - gl_FragCoord.y)
+             + (1.0f - gl_FragCoord.z) * (1.0f - gl_FragCoord.z);
+    fragColor = vec4(1.0f - distSq, 1.5f - gl_FragCoord.z, gl_FragCoord.z - 0.5f, 1.0f);
 }
 
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -176,7 +176,7 @@ public class HelloWorld {
     private void loop() {
 
         // Set the clear color
-        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
         // Run the rendering loop until the user has attempted to close
         // the window or has pressed the ESCAPE key.
