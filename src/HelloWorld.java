20aec06df0f466d70406c3989218234c5c39e78a
Robert Freeman <zarohian@yahoo.com>
2018-06-03 18:32:00 -0400

    Broken generation, first attempt

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,13 @@
+import org.joml.Vector3f;
+import org.joml.Vector3fc;
 import org.lwjgl.*;
 import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
 import org.lwjgl.system.*;
 
 import java.nio.*;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 import static org.lwjgl.glfw.Callbacks.*;
 import static org.lwjgl.glfw.GLFW.*;
@@ -21,6 +25,10 @@ public class HelloWorld {
     private Shader shader;
     // key press test
     private GLFWKeyCallback keyCallback;
+    // vertices generated, with each coordinate grouped in a Vector3f
+    private HashMap<Vector3f, Integer> vertexCoords;
+    // Indices generated, with each face taking up 3 values with 3 consecutive keys
+    private LinkedList<Integer> indices;
 
     public void run() {
         System.out.println("Hello LWJGL " + Version.getVersion() + "!");
@@ -85,8 +93,6 @@ public class HelloWorld {
             );
         } // the stack frame is popped automatically
 
-
-
         // Make the OpenGL context current
         glfwMakeContextCurrent(window);
 
@@ -120,36 +126,27 @@ public class HelloWorld {
         testMesh.setScaleZ(1.0f);
         float goldenRatio = 0.8090170f;
         float zBooster = 0.0f;
-        testMesh.create(new float[] {
 
-                -1 * goldenRatio / 2.0f, 0, 1 / 2.0f + zBooster,
-                goldenRatio / 2.0f, 0, 1 / 2.0f + zBooster,
-                -1 * goldenRatio / 2.0f, 0, -1 / 2.0f + zBooster,
-                goldenRatio / 2.0f, 0, -1 / 2.0f + zBooster,
-                0, 1 / 2.0f, goldenRatio / 2.0f + zBooster,
-                0, 1 / 2.0f, -1 * goldenRatio / 2.0f + zBooster,
-                0, -1 / 2.0f, goldenRatio / 2.0f + zBooster,
-                0, -1 / 2.0f, -1 * goldenRatio / 2.0f + zBooster,
-                1 / 2.0f, goldenRatio / 2.0f, zBooster,
-                -1 / 2.0f, goldenRatio / 2.0f, zBooster,
-                1 / 2.0f, -1 * goldenRatio / 2.0f, zBooster,
-                -1 / 2.0f, -1 * goldenRatio / 2.0f, zBooster
+        // TODO put setup and organization of vertex/index generation here
+        vertexCoords = new HashMap<Vector3f, Integer>();
+        indices = new LinkedList<>();
 
-                /* swapping golden ratio with 1, could be useful
-                -1 / 2.0f, 0, goldenRatio / 2.0f,
-                1 / 2.0f, 0, goldenRatio / 2.0f,
-                -1 / 2.0f, 0, -1 * goldenRatio / 2.0f,
-                1 / 2.0f, 0, -1 * goldenRatio / 2.0f,
-                0, goldenRatio / 2.0f, 1 / 2.0f,
-                0, goldenRatio / 2.0f, -1 / 2.0f,
-                0, -1 * goldenRatio / 2.0f, 1 / 2.0f,
-                0, -1 * goldenRatio / 2.0f, -1 / 2.0f,
-                goldenRatio / 2.0f, 1 / 2.0f, 0,
-                -1 * goldenRatio / 2.0f, 1 / 2.0f, 0,
-                goldenRatio / 2.0f, -1 / 2.0f, 0,
-                -1 * goldenRatio / 2.0f, -1 / 2.0f, 0
-                */
-        }, new int[] {
+        Vector3f[] initialVertices = new Vector3f[]{
+                new Vector3f(-1 * goldenRatio / 2.0f, 0, 1 / 2.0f + zBooster),
+                new Vector3f(goldenRatio / 2.0f, 0, 1 / 2.0f + zBooster),
+                new Vector3f(-1 * goldenRatio / 2.0f, 0, -1 / 2.0f + zBooster),
+                new Vector3f(goldenRatio / 2.0f, 0, -1 / 2.0f + zBooster),
+                new Vector3f(0, 1 / 2.0f, goldenRatio / 2.0f + zBooster),
+                new Vector3f(0, 1 / 2.0f, -1 * goldenRatio / 2.0f + zBooster),
+                new Vector3f(0, -1 / 2.0f, goldenRatio / 2.0f + zBooster),
+                new Vector3f(0, -1 / 2.0f, -1 * goldenRatio / 2.0f + zBooster),
+                new Vector3f(1 / 2.0f, goldenRatio / 2.0f, zBooster),
+                new Vector3f(-1 / 2.0f, goldenRatio / 2.0f, zBooster),
+                new Vector3f(1 / 2.0f, -1 * goldenRatio / 2.0f, zBooster),
+                new Vector3f(-1 / 2.0f, -1 * goldenRatio / 2.0f, zBooster)
+        };
+
+        int[] initialIndices = new int[] {
                 0,4,1,
                 0,9,4,
                 9,5,4,
@@ -170,7 +167,82 @@ public class HelloWorld {
                 9,11,2,
                 9,2,5,
                 7,2,11
-        });
+        };
+
+        for (int i = 0; i < initialIndices.length; i += 3) {
+            generateVertices(
+                    1,
+                    initialVertices[initialIndices[i]],
+                    initialVertices[initialIndices[i + 1]],
+                    initialVertices[initialIndices[i + 2]]
+            );
+        }
+
+        Object[] vertices = vertexCoords.keySet().toArray();
+        float[] finalVertices = new float[vertices.length * 3];
+        int i = 0;
+        for (Object v: vertices) {
+            finalVertices[i] = ((Vector3f) v).x;
+            finalVertices[i + 1] = ((Vector3f) v).y;
+            finalVertices[i + 2] = ((Vector3f) v).z;
+            System.out.println("Coordinate " + i + ": " + finalVertices[i] + ", " + finalVertices[i + 1] + ", " + finalVertices[i + 2]);
+            i += 3;
+        }
+
+        int[] finalIndices = new int[indices.size()];
+        i = 0;
+        while (!indices.isEmpty()) {
+            finalIndices[i] = indices.pop();
+            System.out.println(finalIndices[i]);
+            i++;
+        }
+
+        testMesh.create(finalVertices, finalIndices);
+    }
+
+    private void generateVertices(int n, Vector3f v1, Vector3f v2, Vector3f v3) {
+        if (n <= 1) { // base case; end recursive chain, add 3 indices to queue of indices
+
+            System.out.println(vertexCoords.size());
+
+            if (!vertexCoords.containsKey(v1)) {
+                vertexCoords.put(v1, vertexCoords.size());
+            }
+            if (!vertexCoords.containsKey(v2)) {
+                vertexCoords.put(v2, vertexCoords.size());
+            }
+            if (!vertexCoords.containsKey(v3)) {
+                vertexCoords.put(v3, vertexCoords.size());
+            }
+
+            indices.add(vertexCoords.get(v1));
+            indices.add(vertexCoords.get(v2));
+            indices.add(vertexCoords.get(v3));
+
+            return;
+        }
+
+        Vector3f v4, v5, v6; // new vertices to generate 4 subtriangles per face
+
+        v4 = new Vector3f((v1.x + v2.x) / 2.0f, (v1.y + v2.y) / 2.0f, (v1.z + v2.z) / 2.0f);
+        if (!vertexCoords.containsKey(v4)) {
+            vertexCoords.put(v4, vertexCoords.size());
+        }
+
+        v5 = new Vector3f((v3.x + v2.x) / 2.0f, (v3.y + v2.y) / 2.0f, (v3.z + v2.z) / 2.0f);
+        if (!vertexCoords.containsKey(v5)) {
+            vertexCoords.put(v5, vertexCoords.size());
+        }
+
+        v6 = new Vector3f((v1.x + v3.x) / 2.0f, (v1.y + v3.y) / 2.0f, (v1.z + v3.z) / 2.0f);
+        if (!vertexCoords.containsKey(v6)) {
+            vertexCoords.put(v6, vertexCoords.size());
+        }
+
+        generateVertices(--n, v4, v5, v6);
+        generateVertices(--n, v2, v5, v4);
+        generateVertices(--n, v5, v3, v6);
+        generateVertices(--n, v4, v6, v1);
     }
 
     private void loop() {
